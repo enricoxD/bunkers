036aab5e79e08eb9c33d893e100da2c0a48fc7fd
bestauto <71661047+BestAutoyes@users.noreply.github.com>
2022-10-06 23:41:28 +0200

    Update PlayerRespawn.kt

--- src/main/kotlin/de/hglabor/bunkers/mechanics/PlayerRespawn.kt
+++ src/main/kotlin/de/hglabor/bunkers/mechanics/PlayerRespawn.kt
@@ -2,8 +2,10 @@ package de.hglabor.bunkers.mechanics
 
 import de.hglabor.bunkers.game.GameManager
 import de.hglabor.bunkers.game.phase.phases.EndPhase
+import de.hglabor.bunkers.game.phase.phases.IngamePhase
 import de.hglabor.bunkers.teams.BunkersTeam
 import de.hglabor.hcfcore.Core
+import de.hglabor.hcfcore.event.koth.KothManager
 import de.hglabor.hcfcore.manager.player.teamPlayer
 import net.axay.kspigot.chat.KColors
 import net.axay.kspigot.chat.literalText
@@ -16,6 +18,8 @@ import net.axay.kspigot.runnables.taskRunLater
 import org.bukkit.GameMode
 import org.bukkit.event.EventPriority
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import java.util.*
 
 object PlayerRespawn {
@@ -24,8 +28,11 @@ object PlayerRespawn {
 
     fun enable() {
         listen<PlayerDeathEvent>(priority = EventPriority.LOW) {
+            if (GameManager.currentPhase != IngamePhase) return@listen
             val player = it.player
             val team = player.teamPlayer.team ?: return@listen
+            DeathMessages.announce(it)
+
             respawningPlayers.add(player.uniqueId)
             taskRunLater(1) {
                 player.gameMode = GameMode.SPECTATOR
@@ -62,9 +69,90 @@ object PlayerRespawn {
                 )
             }
         }
+
+        // TODO test and refactor this is SHIT
+
+        listen<PlayerQuitEvent> {
+            if (GameManager.currentPhase != IngamePhase) return@listen
+            val player = it.player
+            val team = player.teamPlayer.team ?: return@listen
+
+            if (team.isRaidable && player.uniqueId in eliminatedPlayers) {
+                return@listen
+            } else if (player.uniqueId in respawningPlayers) {
+                return@listen
+            }
+
+            respawningPlayers.add(player.uniqueId)
+            if (team.dtr <= 0.0f) {
+                eliminatedPlayers.add(player.uniqueId)
+                checkForWinner()
+                return@listen
+            }
+
+            team.dtr -= 1
+            team.notify(false) {
+                text("Member Quit: ") { color = KColors.RED }
+                text(it.player.name)
+                newLine()
+                text("DTR: ") { color = KColors.RED }
+                text(team.dtr.toString())
+            }
+
+            if (team.dtr <= 0.0 && !team.isRaidable) {
+                team.isRaidable = true
+                team.notify {
+                    text("You are now ") { color = KColors.GRAY }
+                    text("raidable") { color = KColors.RED; bold = true }
+                    text("!") { color = KColors.GRAY }
+                    newLine()
+                    text("From now on, your death will be permanent.") { color = KColors.RED }
+                }
+            }
+        }
+
+        listen<PlayerJoinEvent> {
+            if (GameManager.currentPhase != IngamePhase) return@listen
+            val player = it.player
+            val team = player.teamPlayer.team
+
+            player.gameMode = GameMode.SPECTATOR
+            if (team == null) {
+                val koth = KothManager.currentKoth ?: return@listen
+                player.teleport(koth.team.claim.captureZone.centerLocation)
+            } else {
+                player.teamPlayer.team?.homeLocation?.let { location ->
+                    player.teleport(location)
+                }
+            }
+
+            if (player.uniqueId in eliminatedPlayers) {
+                player.title(literalText("You have been eliminated!") { color = KColors.RED })
+            }
+
+            task(true, 20, 20, 5, endCallback = {
+                taskRunLater(20) {
+                    respawningPlayers.remove(player.uniqueId)
+                    player.clearTitle()
+                    player.teamPlayer.team?.homeLocation?.let { location ->
+                        player.teleport(location)
+                    }
+                    player.gameMode = GameMode.SURVIVAL
+                    player.inventory.clear()
+                    player.fireTicks = 0
+                    player.heal()
+                    player.feedSaturate()
+                }
+            }) {
+                player.title(
+                    literalText("${it.counterDownToOne}"),
+                    literalText("Respawning in") { color = KColors.GRAY }
+                )
+            }
+        }
     }
 
-    fun checkForWinner() {
+    private fun checkForWinner() {
         val winner = Core.teamManager.teams.filterIsInstance<BunkersTeam>().singleOrNull { team ->
             team.members.any { uuid -> uuid !in eliminatedPlayers }
         } ?: return
